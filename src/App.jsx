a3e72bdf5c40086b31f8785df781ae96e7bfd9ca
Jason Chan <jasonchanhw0531@gmail.com>
2024-01-09 01:14:00 +0000

    added dice component and did some css

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dice from './components/Dice.jsx'
 
 function App() {
 
   return (
     <main>
-        main
+        <div className = "diceContainer">
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+            <Dice value = {1}/>
+        </div>
     </main>
   )
 }
