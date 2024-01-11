7d46ce5151d011fabae4783ccb537525ce46623e
Jason Chan <jasonchanhw0531@gmail.com>
2024-01-11 12:55:08 +0000

    added holding dice, rerolling and id for component

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,56 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Dice from './components/Dice.jsx'
+import {nanoid} from 'nanoid'
 
 function App() {
+  const [numArr, setDiceNum] = React.useState(allNewDice())
+
+  //completely changes the dice array generating 10 new numbers
+  function allNewDice(){
+    const randomNumArr = []
+    for (let i=0;i<10;i++){
+      randomNumArr.push(
+        {
+          value: Math.floor(Math.random()*6)+1, 
+          isHeld:false,
+          id: nanoid()
+        }
+      );
+    }
+    return randomNumArr
+  }
+
+  //rolling the dice on the button click
+  function rollDice(){
+    setDiceNum(allNewDice())
+  }
+
+  //holding the dice
+  function holdDice(id){
+    //mapping over the previous state array
+    setDiceNum(prevState=>{
+      return prevState.map((die)=>{
+        // finding the dice with the id which matches the one clicked
+        // when found return array but with isHeld property reversed
+        //else return normal die object
+        return (die.id === id) ? {...die,isHeld:!die.isHeld}:die
+      })
+    })
+  }
+
+  const diceElements = numArr.map((num)=>{
+    return (<Dice key= {num.id} id ={num.id} value = {num.value} isHeld={num.isHeld} holdDice = {holdDice}/>)
+  })
+
 
   return (
     <main>
         <div className = "diceContainer">
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
-            <Dice value = {1}/>
+            {diceElements}
         </div>
+        <button className = "reset--button" onClick={rollDice}>Roll</button>
+
     </main>
   )
 }
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Dice(props){
+export default function Dice({value, isHeld,holdDice,id}){
+    const styles = {
+        backgroundColor: isHeld ? "#59E391":"white"
+    }
     return(
-        <div className = 'dice--component'>
-            <b>{props.value}</b>
+        <div className = 'dice--component' style = {styles} onClick={()=>{holdDice(id)}}>
+            <b>{value}</b>
         </div>
     )
 }
